ae33129648c8f13139f09f3c0cbdca609c7ed32d
deepinbubblegum <5911110222@mutacth.com>
2021-06-14 17:04:53 +0700

    update imu gmc

--- src/imu_mpu6050/src/imu_mpu6050_node.cpp
+++ src/imu_mpu6050/src/imu_mpu6050_node.cpp
@@ -15,27 +15,37 @@ int main(int argc, char **argv)
     // create variable init
     serial::Serial ser;
     std::string serial_port;
+    std::string frame_id;
     int buadrate;
+    double time_offset_in_seconds;
+    double linear_acceleration_stddev;
+    double angular_velocity_stddev;
+    double orientation_stddev;
+
+    tf::Quaternion orientation;
+    tf::Quaternion zero_orientation;
 
     // init ros node and set name node
     ros::init(argc, argv, "imu_mpu6050_node");
 
     // node handle must be int manin() FOR GET PARAM
     ros::NodeHandle private_node_handle("~");
+    private_node_handle.param<double>("time_offset_in_seconds", time_offset_in_seconds, 0.0);
+    private_node_handle.param<double>("linear_acceleration_stddev", linear_acceleration_stddev, 0.0);
+    private_node_handle.param<std::string>("frame_id", frame_id, "imu_link");
+    private_node_handle.param<double>("angular_velocity_stddev", angular_velocity_stddev, 0.0);
+    private_node_handle.param<double>("orientation_stddev", orientation_stddev, 0.0);
 
     // getParam from file launch conf
     private_node_handle.getParam("port", serial_port);
     private_node_handle.getParam("buadrate", buadrate);
 
-    // // // show value param get
-    // ROS_INFO("port: %s", serial_port.c_str());
-    // ROS_INFO("port: %d", buadrate);
-
     // HANDLE TOPIC IMU
     ros::NodeHandle nh("imu");
 
     // PUBLISHER TOPIC /imu/data
-    ros::Publisher imu_pub = nh.advertise<std_msgs::String>("data", 50);
+    ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("data", 50);
+    ros::Publisher imu_temperature_pub = nh.advertise<sensor_msgs::Temperature>("temperature", 50);
 
     try
     {
@@ -51,17 +61,25 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    if (ser.isOpen())
+    try
     {
-        ROS_INFO_STREAM("Serial Port initialized");
-        ser.write("a"); //Using the serial port, disable I2C
-        sleep(1);
-        ser.write("c");
-        sleep(1);
+        if (ser.isOpen())
+        {
+            ROS_INFO_STREAM("Serial Port initialized");
+            ser.write("a"); //Using the serial port, disable I2C
+            sleep(1);
+            ser.write("c");
+            sleep(1);
+        }
+        else
+        {
+            return -1;
+        }
     }
-    else
+    catch (serial::IOException &e)
     {
-        return -1;
+        ROS_ERROR_STREAM("Error reading from the serial port " << ser.getPort() << ". Closing connection.");
+        ser.close();
     }
 
     std::string input;
@@ -69,11 +87,30 @@ int main(int argc, char **argv)
     char check_Sum;
     int data_packet_start;
     int data_packet_identify;
-    float _ax, _ay, _az,
+
+    double _ax, _ay, _az,
         _wx, _wy, _wz,
         roll, pitch, yaw, T;
 
     ros::Rate loop_rate(200);
+
+    sensor_msgs::Imu imu;
+
+    imu.linear_acceleration_covariance[0] = linear_acceleration_stddev;
+    imu.linear_acceleration_covariance[4] = linear_acceleration_stddev;
+    imu.linear_acceleration_covariance[8] = linear_acceleration_stddev;
+
+    imu.angular_velocity_covariance[0] = angular_velocity_stddev;
+    imu.angular_velocity_covariance[4] = angular_velocity_stddev;
+    imu.angular_velocity_covariance[8] = angular_velocity_stddev;
+
+    imu.orientation_covariance[0] = orientation_stddev;
+    imu.orientation_covariance[4] = orientation_stddev;
+    imu.orientation_covariance[8] = orientation_stddev;
+
+    sensor_msgs::Temperature temperature_msg;
+    temperature_msg.variance = 0;
+
     while (ros::ok())
     {
         ros::spinOnce();
@@ -83,10 +120,6 @@ int main(int argc, char **argv)
             // std_msgs::String result;
             read = ser.read(ser.available());
             input += read;
-            // ROS_INFO_STREAM(read);
-            // if (input.length() == 10){
-            //     ROS_INFO_STREAM(read);
-            // }else
             if (input.length() > 10)
             {
                 if (input[0] == 0x55)
@@ -97,42 +130,70 @@ int main(int argc, char **argv)
                     case 0x51:
                         if ((check_Sum + 0x51) == input[10])
                         {
-                            _ax = ((float(short((input[3] << 8) | input[2]))) / 32768) * 16;
-                            _ay = ((float(short((input[5] << 8) | input[4]))) / 32768) * 16;
-                            _az = ((float(short((input[7] << 8) | input[6]))) / 32768) * 16;
+                            // linear accleration
+                            _ax = ((double(short((input[3] << 8) | input[2]))) / 32768) * 16;
+                            _ay = ((double(short((input[5] << 8) | input[4]))) / 32768) * 16;
+                            _az = ((double(short((input[7] << 8) | input[6]))) / 32768) * 16;
                             T = (short((input[9] << 8) | input[8]) / 340) + 36.53;
                         }
                         break;
                     case 0x52:
                         if ((check_Sum + 0x52) == input[10])
                         {
-                            _wx = ((float(short((input[3] << 8) | input[2]))) / 32768) * 2000;
-                            _wy = ((float(short((input[5] << 8) | input[4]))) / 32768) * 2000;
-                            _wz = ((float(short((input[7] << 8) | input[6]))) / 32768) * 2000;
+                            //
+                            _wx = ((double(short((input[3] << 8) | input[2]))) / 32768) * 2000;
+                            _wy = ((double(short((input[5] << 8) | input[4]))) / 32768) * 2000;
+                            _wz = ((double(short((input[7] << 8) | input[6]))) / 32768) * 2000;
                             T = (short((input[9] << 8) | input[8]) / 340) + 36.53;
                         }
                         break;
                     case 0x53:
                         if ((check_Sum + 0x53) == input[10])
                         {
-                            roll = ((float(short((input[3] << 8) | input[2]))) / 32768) * 180;
-                            pitch = ((float(short((input[5] << 8) | input[4]))) / 32768) * 180;
-                            yaw = ((float(short((input[7] << 8) | input[6]))) / 32768) * 180;
+                            // angular_velocity
+                            roll = ((double(short((input[3] << 8) | input[2]))) / 32768) * 180;
+                            pitch = ((double(short((input[5] << 8) | input[4]))) / 32768) * 180;
+                            yaw = ((double(short((input[7] << 8) | input[6]))) / 32768) * 180;
                             T = (short((input[9] << 8) | input[8]) / 340) + 36.53;
                         }
                         break;
                     }
-
-                    ROS_INFO_STREAM("T:" << T);
                 }
-
+                input.erase(0);
                 ROS_INFO("ax :%f ay :%f az : %f wx : %f wy: %f wz: %f roll: %f picth: %f yaw: %f T: %f", _ax, _ay, _az, _wx, _wy, _wz, roll, pitch, yaw, T);
 
-                input.erase(0);
+                // calculate measurement time
+                ros::Time measurement_time = ros::Time::now() + ros::Duration(time_offset_in_seconds);
+
+                // publish imu message
+                imu.header.stamp = measurement_time;
+                imu.header.frame_id = frame_id;
+
+                imu.angular_velocity.x = roll;
+                imu.angular_velocity.y = pitch;
+                imu.angular_velocity.z = yaw;
+
+                imu.linear_acceleration.x = _ax;
+                imu.linear_acceleration.y = _ay;
+                imu.linear_acceleration.z = _az;
+
+                // imu.orientation.x = roll;
+                // imu.orientation.y = pitch;
+                // imu.orientation.z = yaw;
+
+                imu_pub.publish(imu);
+
+                // publish temperature message
+                temperature_msg.header.stamp = measurement_time;
+                temperature_msg.header.frame_id = frame_id;
+                temperature_msg.temperature = T;
+
+                imu_temperature_pub.publish(temperature_msg);
             }
         }
 
-        //loop rate
+        //loop
+        ros::spinOnce();
         loop_rate.sleep();
     }
 }
